e63f62f86eed9000cb15e1d7e38c1416dbfcf851
crapdev <calborparra@gmail.com>
2026-08-12 20:22:52 -0500

    .

--- banco/stockflow/src/main/java/espumasYMas/repository/RepositoryProduct.java
+++ banco/stockflow/src/main/java/espumasYMas/repository/RepositoryProduct.java
@@ -8,13 +8,42 @@ import java.util.ArrayList;
  * @author cohorte_5
  */
 public class RepositoryProduct {
-    private ArrayList <Product> products;
     
+    private ArrayList <Product> products;
     
     public RepositoryProduct(){
         this.products = new ArrayList<>();
     }
     
     
+    //Methods
+    public void save(Product product){
+        products.add(product);
+    }
     
+    public ArrayList<Product> showProducts(){
+        return products;
+    }
+    
+    public Product findByCode(String code){
+        for (Product product : products) {
+            if (product.getCode().equalsIgnoreCase(code)) {
+                return product;
+            }
+        }
+        return null;
+    }
+    
+    public boolean existsByCode(String code){
+        return findByCode(code) != null;
+    }
+    
+    public boolean deleteByCode(String code){
+        Product product = findByCode(code);
+        
+        if (product == null) {
+            return false;
+        }
+        return products.remove(product);
+    }
 }
